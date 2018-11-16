31155d822852eb8ee2ebc5f375425a5d2d93b960
bl4ckscor3 <donalddani9612@gmx.de>
2018-11-16 18:03:59 +0100

    complete person access

--- src/main/java/de/tudarmstadt/informatik/ukp/athenakp/database/access/PersonCommonAccess.java
+++ src/main/java/de/tudarmstadt/informatik/ukp/athenakp/database/access/PersonCommonAccess.java
@@ -1,8 +1,9 @@
 package de.tudarmstadt.informatik.ukp.athenakp.database.access;
 
-import de.tudarmstadt.informatik.ukp.athenakp.database.models.Person;
-
+import java.util.Date;
 import java.util.List;
+
+import de.tudarmstadt.informatik.ukp.athenakp.database.models.Person;
 
 /**
  * This interface is meant to hide the database layer. For any new use case involving
@@ -12,11 +13,59 @@ import java.util.List;
  */
 public interface PersonCommonAccess extends CommonAccess<Person> {
 
-    /**
-     * Get all persons with specified last name.
-     * @param name The person's last name.
-     * @return A List of all persons with the specified last name.
-     */
-    public List<Person> getByLastName(String name);
+	/**
+	 * Get all persons with specified person id.
+	 * @param prefix The person's person id.
+	 * @return A List of all persons with the specified person id.
+	 */
+	public List<Person> getByPersonID(Long personID);
 
+	/**
+	 * Get all persons with specified prefix.
+	 * @param prefix The person's prefix.
+	 * @return A List of all persons with the specified prefix.
+	 */
+	public List<Person> getByPrefix(String prefix);
+
+	/**
+	 * Get all persons with specified last first name.
+	 * @param firstName The person's first name.
+	 * @return A List of all persons with the specified first name.
+	 */
+	public List<Person> getByFirstName(String firstName);
+
+	/**
+	 * Get all persons with specified middle name.
+	 * @param middleName The person's middle name.
+	 * @return A List of all persons with the specified middle name.
+	 */
+	public List<Person> getByMiddleName(String middleName);
+
+	/**
+	 * Get all persons with specified last name.
+	 * @param lastName The person's last name.
+	 * @return A List of all persons with the specified last name.
+	 */
+	public List<Person> getByLastName(String lastName);
+
+	/**
+	 * Get all persons with specified birthdate.
+	 * @param birthdate The person's birthdate.
+	 * @return A List of all persons with the specified birthdate.
+	 */
+	public List<Person> getByBirthdate(Date birthdate);
+
+	/**
+	 * Get all persons with specified obit.
+	 * @param obit The person's obit.
+	 * @return A List of all persons with the specified obit.
+	 */
+	public List<Person> getByObit(Date obit);
+
+	/**
+	 * Get all persons with specified institution's id.
+	 * @param institutionID The person's institution's id.
+	 * @return A List of all persons with the specified institution's id.
+	 */
+	public List<Person> getByInstitutionID(String institutionID);
 }
--- src/main/java/de/tudarmstadt/informatik/ukp/athenakp/database/access/PersonHibernateAccess.java
+++ src/main/java/de/tudarmstadt/informatik/ukp/athenakp/database/access/PersonHibernateAccess.java
@@ -1,77 +1,154 @@
 package de.tudarmstadt.informatik.ukp.athenakp.database.access;
 
-import de.tudarmstadt.informatik.ukp.athenakp.database.HibernateUtils;
-import de.tudarmstadt.informatik.ukp.athenakp.database.models.Person;
+import java.util.Date;
+import java.util.List;
+
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
-import java.util.List;
+import de.tudarmstadt.informatik.ukp.athenakp.database.HibernateUtils;
+import de.tudarmstadt.informatik.ukp.athenakp.database.models.Person;
 
 /**
  * Implementation of PersonCommonAccess with Hibernate.
  *
- * @author Tristan Wettich
+ * @author Tristan Wettich, Daniel Lehmann
  */
 public class PersonHibernateAccess implements PersonCommonAccess {
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public List<Person> getByLastName(String name) {
-        Session session = HibernateUtils.getSessionFactory().openSession();
-        Criteria criteria = session.createCriteria(Person.class);
-        criteria.add(Restrictions.eq("lastName", name));
-        List<Person> result = criteria.list();
-        session.close();
-        return result;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<Person> getByPersonID(Long personID)
+	{
+		return getByRestriction("personID", personID);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void add(Person data) {
-        Session session = HibernateUtils.getSessionFactory().openSession();
-        session.beginTransaction();
-        session.save(data);
-        session.getTransaction().commit();
-        session.close();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<Person> getByPrefix(String prefix)
+	{
+		return getByRestriction("prefix", prefix);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void update(Person data) {
-        Session session = HibernateUtils.getSessionFactory().openSession();
-        session.beginTransaction();
-        session.update(data);
-        session.getTransaction().commit();
-        session.close();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<Person> getByFirstName(String firstName)
+	{
+		return getByRestriction("firstName", firstName);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void delete(Person data) {
-        Session session = HibernateUtils.getSessionFactory().openSession();
-        session.beginTransaction();
-        session.remove(data);
-        session.getTransaction().commit();
-        session.close();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<Person> getByMiddleName(String middleName)
+	{
+		return getByRestriction("middleName", middleName);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public List<Person> get() {
-        Session session = HibernateUtils.getSessionFactory().openSession();
-        List<Person> result = session.createCriteria(Person.class).list();
-        session.close();
-        return result;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<Person> getByLastName(String lastName)
+	{
+		return getByRestriction("lastName", lastName);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<Person> getByBirthdate(Date birthdate)
+	{
+		return getByRestriction("birthdate", birthdate);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<Person> getByObit(Date obit)
+	{
+		return getByRestriction("obit", obit);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<Person> getByInstitutionID(String institutionID) //TODO
+	{
+		return null;
+	}
+
+	/**
+	 * Common code used by all get methods above
+	 * @param name The name of the column to restrict
+	 * @param value The value to restrict the selection to
+	 * @return A List of all persons with the given restriction
+	 */
+	private List<Person> getByRestriction(String name, Object value)
+	{
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		Criteria criteria = session.createCriteria(Person.class);
+		criteria.add(Restrictions.eq(name, value));
+		List<Person> result = criteria.list();
+		session.close();
+		return result;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void add(Person data) {
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		session.beginTransaction();
+		session.save(data);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void update(Person data) {
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		session.beginTransaction();
+		session.update(data);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void delete(Person data) {
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		session.beginTransaction();
+		session.remove(data);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<Person> get() {
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		List<Person> result = session.createCriteria(Person.class).list();
+		session.close();
+		return result;
+	}
 }
