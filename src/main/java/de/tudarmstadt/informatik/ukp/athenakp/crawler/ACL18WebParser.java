c9b8d4fcef704d6e0a9b008d1e4ce660678bbc72
bl4ckscor3 <donalddani9612@gmx.de>
2018-12-07 13:40:00 +0100

    fix syntax error

--- src/main/java/de/tudarmstadt/informatik/ukp/athenakp/crawler/ACL18WebParser.java
+++ src/main/java/de/tudarmstadt/informatik/ukp/athenakp/crawler/ACL18WebParser.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -14,8 +13,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import de.tudarmstadt.informatik.ukp.athenakp.database.models.Conference;
 
 import de.tudarmstadt.informatik.ukp.athenakp.crawler.CrawlerToolset.PaperStore;
 import de.tudarmstadt.informatik.ukp.athenakp.database.models.Conference;
@@ -139,7 +136,36 @@ class ACL18WebParser extends AbstractCrawler{
 
 	@Override
 	public ArrayList<ArrayList<String>> getPaperAuthor() throws IOException {
-		return extractPaperAuthor(fetchWebpages(startURLPaper));
+		System.out.println("Fetching webpages...");
+		List<Document> webpages = fetchWebpages(startURLPaper);
+		System.out.println("Preparing data and starting 4 scraper threads...");
+		int quarterSize = (int)Math.ceil(webpages.size() / 4);
+		List<Document> input1 = webpages.subList(0, quarterSize);
+		List<Document> input2 = webpages.subList(quarterSize, quarterSize * 2);
+		List<Document> input3 = webpages.subList(quarterSize * 2, quarterSize * 3);
+		List<Document> input4 = webpages.subList(quarterSize * 3, webpages.size());
+		ArrayList<ArrayList<String>> result = new ArrayList<>();
+		ExecutorService executor = Executors.newFixedThreadPool(4);
+		Future<ArrayList<ArrayList<String>>> f1 = executor.submit(() -> {return extractPaperAuthor(input1);});
+		Future<ArrayList<ArrayList<String>>> f2 = executor.submit(() -> {return extractPaperAuthor(input2);});
+		Future<ArrayList<ArrayList<String>>> f3 = executor.submit(() -> {return extractPaperAuthor(input3);});
+		Future<ArrayList<ArrayList<String>>> f4 = executor.submit(() -> {return extractPaperAuthor(input4);});
+		System.out.println("Waiting for thread results...");
+
+		try {
+			result.addAll(f1.get());
+			result.addAll(f2.get());
+			result.addAll(f3.get());
+			result.addAll(f4.get());
+			System.out.println("Gathered all results!");
+		}
+		catch(InterruptedException | ExecutionException e) {
+			System.err.println("Error while gathering results!");
+			e.printStackTrace();
+		}
+
+		executor.shutdown();
+		return result;
 	}
 
 	/**
@@ -255,40 +281,6 @@ class ACL18WebParser extends AbstractCrawler{
 		return currentConference;
 	}
 
-	@Override
-	public ArrayList<ArrayList<String>> getPaperAuthor() throws IOException {
-		System.out.println("Fetching webpages...");
-		List<Document> webpages = fetchWebpages(startURLPaper);
-		System.out.println("Preparing data and starting 4 scraper threads...");
-		int quarterSize = (int)Math.ceil(webpages.size() / 4);
-		List<Document> input1 = webpages.subList(0, quarterSize);
-		List<Document> input2 = webpages.subList(quarterSize, quarterSize * 2);
-		List<Document> input3 = webpages.subList(quarterSize * 2, quarterSize * 3);
-		List<Document> input4 = webpages.subList(quarterSize * 3, webpages.size());
-		ArrayList<ArrayList<String>> result = new ArrayList<>();
-		ExecutorService executor = Executors.newFixedThreadPool(4);
-		Future<ArrayList<ArrayList<String>>> f1 = executor.submit(() -> {return extractPaperAuthor(input1);});
-		Future<ArrayList<ArrayList<String>>> f2 = executor.submit(() -> {return extractPaperAuthor(input2);});
-		Future<ArrayList<ArrayList<String>>> f3 = executor.submit(() -> {return extractPaperAuthor(input3);});
-		Future<ArrayList<ArrayList<String>>> f4 = executor.submit(() -> {return extractPaperAuthor(input4);});
-		System.out.println("Waiting for thread results...");
-
-		try {
-			result.addAll(f1.get());
-			result.addAll(f2.get());
-			result.addAll(f3.get());
-			result.addAll(f4.get());
-			System.out.println("Gathered all results!");
-		}
-		catch(InterruptedException | ExecutionException e) {
-			System.err.println("Error while gathering results!");
-			e.printStackTrace();
-		}
-
-		executor.shutdown();
-		return result;
-	}
-	
 	@Override
 	public ArrayList<ArrayList<String>> getTimetable() throws IOException {
 		ArrayList<ArrayList<String>> result = new ArrayList<>();
