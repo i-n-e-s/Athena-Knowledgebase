d100a50fa91149538a97d05b27d87b05ea5d70f2
bl4ckscor3 <donalddani9612@gmx.de>
2019-01-26 19:58:59 +0100

    better name

--- src/main/java/de/tudarmstadt/informatik/ukp/athenakp/api/APIController.java
+++ src/main/java/de/tudarmstadt/informatik/ukp/athenakp/api/APIController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.HandlerMapping;
 
 @RestController
-public class APIConnector {
+public class APIController {
 	@RequestMapping("/**") //matches the complete path (containing all subpaths), just make sure that there are no ? in there!!
 	public String/*List<Model>*/ apiConnector(HttpServletRequest request){ //the argument contains everything that was not matched to any other argument
 		return request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString(); //returns the complete url sans the domain
