522b30296807f2190e806bc37c8c8a852c540113
bl4ckscor3 <donalddani9612@gmx.de>
2019-01-27 22:05:31 +0100

    add another test

--- src/test/java/de/tudarmstadt/informatik/ukp/athenakp/api/RequestScannerTest.java
+++ src/test/java/de/tudarmstadt/informatik/ukp/athenakp/api/RequestScannerTest.java
@@ -11,12 +11,13 @@ import org.junit.Test;
 import de.tudarmstadt.informatik.ukp.athenakp.api.RequestToken.RequestTokenType;
 
 public class RequestScannerTest {
-	private RequestScanner uut1, uut2;
+	private RequestScanner uut1, uut2, uut3;
 
 	@Before
 	public void setup() {
 		uut1 = new RequestScanner("/paper:Author=Daniel+Klingbein&Topic=vogonpoetry$author:Obit=1993+05+22"); //syntactically correct
 		uut2 = new RequestScanner("/paper:Author=Daniel+Klingbein&Topic|vogonpoetry$author:Obit=1993+05+22"); //syntactically incorrect, unknown symbol in the middle (|)
+		uut3 = new RequestScanner("/paper:Author=Daniel+Klingbein&Topic&vogonpoetry$author:Obit=1993+05+22"); //syntactically incorrect, symbol at incorrect position (& instead of = in the middle)
 	}
 
 	@Test
@@ -70,6 +71,40 @@ public class RequestScannerTest {
 		expected.add(new RequestToken(RequestTokenType.ATTR_SEPERATOR, "&", 30));
 		expected.add(new RequestToken(RequestTokenType.NAME, "Topic", 31));
 		expected.add(new RequestToken(RequestTokenType.ERROR, "|", 36));
+		expected.add(new RequestToken(RequestTokenType.NAME, "vogonpoetry", 37));
+		expected.add(new RequestToken(RequestTokenType.JOIN, "$", 48));
+		expected.add(new RequestToken(RequestTokenType.NAME, "author", 49));
+		expected.add(new RequestToken(RequestTokenType.ATTR_SPECIFIER, ":", 55));
+		expected.add(new RequestToken(RequestTokenType.NAME, "Obit", 56));
+		expected.add(new RequestToken(RequestTokenType.ATTR_EQ, "=", 60));
+		expected.add(new RequestToken(RequestTokenType.NUMBER, "1993", 61));
+		expected.add(new RequestToken(RequestTokenType.SPACE, "+", 65));
+		expected.add(new RequestToken(RequestTokenType.NUMBER, "05", 66));
+		expected.add(new RequestToken(RequestTokenType.SPACE, "+", 68));
+		expected.add(new RequestToken(RequestTokenType.NUMBER, "22", 69));
+		expected.add(new RequestToken(RequestTokenType.END, "<end>", 71));
+
+		assertEquals("Actual size is not equal to expected size!", expected.size(), actual.size());
+
+		while(expected.peek() != null) {
+			assertEquals("Element at position " + i++ + " is not the same!", expected.poll(), actual.poll());
+		}
+
+		////////////
+
+		expected = new ArrayDeque<>();
+		actual = uut3.scan();
+		expected.add(new RequestToken(RequestTokenType.HIERARCHY_SEPERATOR, "/", 0));
+		expected.add(new RequestToken(RequestTokenType.NAME, "paper", 1));
+		expected.add(new RequestToken(RequestTokenType.ATTR_SPECIFIER, ":", 6));
+		expected.add(new RequestToken(RequestTokenType.NAME, "Author", 7));
+		expected.add(new RequestToken(RequestTokenType.ATTR_EQ, "=", 13));
+		expected.add(new RequestToken(RequestTokenType.NAME, "Daniel", 14));
+		expected.add(new RequestToken(RequestTokenType.SPACE, "+", 20));
+		expected.add(new RequestToken(RequestTokenType.NAME, "Klingbein", 21));
+		expected.add(new RequestToken(RequestTokenType.ATTR_SEPERATOR, "&", 30));
+		expected.add(new RequestToken(RequestTokenType.NAME, "Topic", 31));
+		expected.add(new RequestToken(RequestTokenType.ATTR_SEPERATOR, "&", 36));
 		expected.add(new RequestToken(RequestTokenType.NAME, "vogonpoetry", 37));
 		expected.add(new RequestToken(RequestTokenType.JOIN, "$", 48));
 		expected.add(new RequestToken(RequestTokenType.NAME, "author", 49));
