da12e4e18da57d2298677854f8442c9926462c41
bl4ckscor3 <donalddani9612@gmx.de>
2019-02-11 16:59:27 +0100

    fix person entity not being mapped

--- src/main/java/de/tudarmstadt/informatik/ukp/athena/knowledgebase/database/models/Person.java
+++ src/main/java/de/tudarmstadt/informatik/ukp/athena/knowledgebase/database/models/Person.java
@@ -6,16 +6,11 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
@@ -27,11 +22,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 /**
  * @author Tristan Wettich
  */
-@Entity(name = "person")
+@Entity
 @Table(name = "person")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.STRING)
-@DiscriminatorValue(value = "person")
 public class Person extends Model {
 	/*Unique id*/
 	@Id
