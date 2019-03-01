1b2084c0ecb96415ae3c6bff761a9ec9b85d1ee9
bl4ckscor3 <donalddani9612@gmx.de>
2019-03-01 19:30:32 +0100

    remove redundant has numerical fields constraint for verifier
    every model has a numerical attribute: its id

--- src/main/java/de/tudarmstadt/informatik/ukp/athena/knowledgebase/api/RequestVerifier.java
+++ src/main/java/de/tudarmstadt/informatik/ukp/athena/knowledgebase/api/RequestVerifier.java
@@ -107,8 +107,6 @@ public class RequestVerifier {
 			else if(previousEntityName != null && (!SET_ATTRIBUTES.containsKey(previousEntityName) || !SET_ATTRIBUTES.get(previousEntityName).containsKey(entityName)))
 				throw new VerificationFailedException("Entity " + previousEntityName + " does not have a hierarchical relationship with entity " + entityName + "!");
 
-			boolean hasNumericalFields = NUMERICAL_ATTRIBUTES.containsKey(entityName);
-
 			previousEntityName = entityName;
 
 			//loop through the attributes and check each attribute's value of validity
@@ -120,9 +118,7 @@ public class RequestVerifier {
 					throw new VerificationFailedException("Unknown attribute " + attrName + " for entity " + entityName + "!");
 
 				//check correct value
-				if(!hasNumericalFields && attr instanceof NumberAttributeNode)
-					throw new VerificationFailedException("Expected a string for attribute " + attrName + " but got " + ((NumberAttributeNode)attr).valuesToString());
-				else if(hasNumericalFields && attr instanceof NumberAttributeNode) {
+				if(attr instanceof NumberAttributeNode) {
 					//numerical attribute found, but should be a string attribute
 					if(!entityContainsNumericalField(entityName, attrName))
 						throw new VerificationFailedException("Expected a string for attribute " + attrName + " but got " + ((NumberAttributeNode)attr).valuesToString());
@@ -130,7 +126,7 @@ public class RequestVerifier {
 					else if(((NumberAttributeNode)attr).getNumbers().size() != NUMERICAL_ATTRIBUTES.get(entityName).get(attrName))
 						throw new VerificationFailedException("Unexpected amount of numbers given for attribute " + attrName + ". " + "Got " + ((NumberAttributeNode)attr).getNumbers().size() + ", need " + NUMERICAL_ATTRIBUTES.get(entityName).get(attrName));
 				}
-				else if(hasNumericalFields && attr instanceof StringAttributeNode) {
+				else if(attr instanceof StringAttributeNode) {
 					//string attribute found, but should be a numerical attribute
 					if(entityContainsNumericalField(entityName, attrName))
 						throw new VerificationFailedException("Expected " + NUMERICAL_ATTRIBUTES.get(entityName).get(attrName) +" number(s) for attribute " + attrName + " but got " + ((StringAttributeNode)attr).getValue().getString());
