b5de69d6fd57c021a6fe761a46f725b3313b689d
bl4ckscor3 <donalddani9612@gmx.de>
2019-03-12 18:27:52 +0100

    add tests

--- src/test/java/de/tudarmstadt/informatik/ukp/athena/knowledgebase/database/JPATestdatabase.java
+++ src/test/java/de/tudarmstadt/informatik/ukp/athena/knowledgebase/database/JPATestdatabase.java
@@ -1,6 +1,7 @@
 package de.tudarmstadt.informatik.ukp.athena.knowledgebase.database;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
 import java.util.TimeZone;
@@ -16,10 +17,14 @@ import de.tudarmstadt.informatik.ukp.athena.knowledgebase.database.jpa.Instituti
 import de.tudarmstadt.informatik.ukp.athena.knowledgebase.database.jpa.PaperJPAAccess;
 import de.tudarmstadt.informatik.ukp.athena.knowledgebase.database.jpa.PersistenceManager;
 import de.tudarmstadt.informatik.ukp.athena.knowledgebase.database.jpa.PersonJPAAccess;
+import de.tudarmstadt.informatik.ukp.athena.knowledgebase.database.jpa.SessionJPAAccess;
+import de.tudarmstadt.informatik.ukp.athena.knowledgebase.database.jpa.WorkshopJPAAccess;
 import de.tudarmstadt.informatik.ukp.athena.knowledgebase.database.models.Conference;
 import de.tudarmstadt.informatik.ukp.athena.knowledgebase.database.models.Institution;
 import de.tudarmstadt.informatik.ukp.athena.knowledgebase.database.models.Paper;
 import de.tudarmstadt.informatik.ukp.athena.knowledgebase.database.models.Person;
+import de.tudarmstadt.informatik.ukp.athena.knowledgebase.database.models.Session;
+import de.tudarmstadt.informatik.ukp.athena.knowledgebase.database.models.Workshop;
 
 @SpringBootApplication
 public class JPATestdatabase {
@@ -27,12 +32,15 @@ public class JPATestdatabase {
 	private int institutionQuantity;
 	private int authorQuantity;
 	private int paperQuantity;
-	private int eventQuantity;
+	private int sessionQuantity;
+	private int workshopQuantity;
 
 	Conference conferences[];
 	Institution institutions[];
 	Person authors[];
 	Paper papers[];
+	Session sessions[];
+	Workshop workshops[];
 
 	public JPATestdatabase() {
 		setDefaultParameters();
@@ -57,19 +65,20 @@ public class JPATestdatabase {
 		institutionQuantity = 10;
 		authorQuantity = 100;
 		paperQuantity = 50;
-		eventQuantity = 20;
+		sessionQuantity = 20; //TODO: when session part is here, set equal to its quantity
+		workshopQuantity = sessionQuantity;
 	}
 
 	/**
 	 * Creates a database for testing purposes. The created entries are deterministic based on the given parameters.
-	 * The set 
-	 * <b>WARNING:</b> This method deletes the whole database 'athena' and replaces it! If you just want to insert new 
+	 * The set
+	 * <b>WARNING:</b> This method deletes the whole database 'athena' and replaces it! If you just want to insert new
 	 * data use {@link #generateData()} and {@link #insertData()}
 	 */
 	public void createDB() {
 		deleteOldData();
 		generateData();
-		insertData();		
+		insertData();
 	}
 
 	/**
@@ -102,11 +111,13 @@ public class JPATestdatabase {
 		institutions = new Institution[institutionQuantity];
 		authors = new Person[authorQuantity];
 		papers = new Paper[paperQuantity];
+		sessions = new Session[sessionQuantity];
+		workshops = new Workshop[workshopQuantity];
 
 		for(int i = 0; i< conferences.length;i++) {
 			conferences[i] = new Conference();
 			conferences[i].setName("Conference" + i);
-			LocalDate tmpDate = LocalDate.of(1960 + i, (i%12)+1 , (i%28)+1); 
+			LocalDate tmpDate = LocalDate.of(1960 + i, (i%12)+1 , (i%28)+1);
 			conferences[i].setBegin(tmpDate);
 			conferences[i].setEnd(tmpDate.plusDays(1));
 			conferences[i].setCountry("Testcountry" + i);
@@ -142,6 +153,24 @@ public class JPATestdatabase {
 			papers[i].setAnthology("Ant" + i%25);
 
 		}
+
+		for(int i = 0; i < sessions.length; i++) {
+			sessions[i] = new Session();
+			sessions[i].setTitle("Title" + i);
+		}
+
+		for(int i = 0; i < workshops.length; i++) {
+			HashSet<Session> sessions = new HashSet<Session>();
+
+			sessions.add(this.sessions[i]);
+			workshops[i] = new Workshop();
+			workshops[i].setAbbreviation("Abbr" + i);
+			workshops[i].setBegin(LocalDateTime.of(1960 + i, (i%12)+1 , (i%28)+1, i,i%12+1,i%28+1));
+			workshops[i].setEnd(workshops[i].getBegin().plusHours(6));
+			workshops[i].setPlace("Place" + i);
+			workshops[i].setSessions(sessions);
+			workshops[i].setTitle("Title" + i);
+		}
 		System.out.println("Done creating data");
 	}
 
@@ -153,13 +182,16 @@ public class JPATestdatabase {
 		InstitutionJPAAccess ijpaa = new InstitutionJPAAccess();
 		PaperJPAAccess pajpaa = new PaperJPAAccess();
 		PersonJPAAccess pejpaa = new PersonJPAAccess();
-		//TODO add EventJPAAccess here
+		SessionJPAAccess sessjpaa = new SessionJPAAccess();
+		WorkshopJPAAccess wjpaa = new WorkshopJPAAccess();
 		System.out.println("Start inserting Data");
 
 		for (Conference c : conferences) cjpaa.add(c);
 		for (Institution i : institutions) ijpaa.add(i);
 		for (Person a : authors) pejpaa.add(a);
 		for (Paper p: papers)pajpaa.add(p);
+		for (Session s: sessions)sessjpaa.add(s);
+		for (Workshop w: workshops)wjpaa.add(w);
 		System.out.println("Done inserting data");
 	}
 	/**
@@ -181,11 +213,11 @@ public class JPATestdatabase {
 
 	/**
 	 * checks if an author with the same name already exists
-	 * 
+	 *
 	 * @param a the author to be searched in the database
 	 * @return true if an author-entry with the same name exists
 	 */
-/*	private boolean authorInDB(Author a) {
+	/*	private boolean authorInDB(Author a) {
 		Session session = HibernateUtils.getSessionFactory().openSession();
 		List<String> result = session.createSQLQuery(
 				String.format("\nSELECT fullName FROM person \n WHERE fullName = '%s' \n LIMIT 1", a.getFullName() )).list();
@@ -195,11 +227,11 @@ public class JPATestdatabase {
 
 	/**
 	 * checks if a paper with the same title already exists
-	 * 
+	 *
 	 * @param a the paper to be searched in the database
 	 * @return true if a paper-entry exists with the same title
 	 */
-/*	private boolean paperInDB(Paper p) {
+	/*	private boolean paperInDB(Paper p) {
 		Session session = HibernateUtils.getSessionFactory().openSession();
 		List<String> result = session.createSQLQuery(
 				String.format("\nSELECT title FROM paper \n WHERE title = '%s' \n LIMIT 1", p.getTitle() )).list();
@@ -209,7 +241,7 @@ public class JPATestdatabase {
 
 	/**
 	 * conferenceQuantity is the number of Conferences, which will be generated
-	 * 
+	 *
 	 * @return The current ConferenceQuantity
 	 */
 	public int getConferenceQuantity() {
@@ -218,7 +250,7 @@ public class JPATestdatabase {
 
 	/**
 	 * conferenceQuantity is the number of Conferences, which will be generated
-	 * 
+	 *
 	 * @param conferenceQuantity The desired ConferenceQuantity
 	 */
 	public void setConferenceQuantity(int conferenceQuantity) {
@@ -227,7 +259,7 @@ public class JPATestdatabase {
 
 	/**
 	 * institutionQuantity is the number of Institutions, which will be generated
-	 * 
+	 *
 	 * @return The current institutionQuantity
 	 */
 	public int getInstitutionQuantity() {
@@ -236,7 +268,7 @@ public class JPATestdatabase {
 
 	/**
 	 * institutionQuantity is the number of Institution, which will be generated
-	 * 
+	 *
 	 * @param institutionQuantity The desired institutionQuantity
 	 */
 	public void setInstitutionQuantity(int institutionQuantity) {
@@ -245,7 +277,7 @@ public class JPATestdatabase {
 
 	/**
 	 * authorQuantity is the number of Authors, which will be generated
-	 * 
+	 *
 	 * @return authorQuantity The current authorQuantity
 	 */
 	public int getAuthorQuantity() {
@@ -254,7 +286,7 @@ public class JPATestdatabase {
 
 	/**
 	 * paperQuantity is the number of Papers, which will be generated
-	 * 
+	 *
 	 * @param authorQuantity The desired paperQuantity
 	 */
 	public void setAuthorQuantity(int authorQuantity) {
@@ -263,7 +295,7 @@ public class JPATestdatabase {
 
 	/**
 	 * paperQuantity is the number of Papers, which will be generated
-	 * 
+	 *
 	 * @return paperQuantity The current paperQuantity
 	 */
 	public int getPaperQuantity() {
@@ -272,7 +304,7 @@ public class JPATestdatabase {
 
 	/**
 	 * paperQuantity is the number of Papers, which will be generated
-	 * 
+	 *
 	 * @param paperQuantity The desired paperQuantity
 	 */
 	public void setPaperQuantity(int paperQuantity) {
@@ -280,26 +312,44 @@ public class JPATestdatabase {
 	}
 
 	/**
-	 * eventQuantity is the number of Events, which will be generated
-	 * 
-	 * @return The current eventQuantity
+	 * sessionQuantity is the number of Sessions, which will be generated
+	 *
+	 * @return The current sessionQuantity
 	 */
-	public int getEventQuantity() {
-		return eventQuantity;
+	public int getSessionQuantity() {
+		return sessionQuantity;
 	}
 
 	/**
-	 * eventQuantity is the number of Events, which will be generated
-	 * 
-	 * @param eventQuantity The desired eventQuantity
+	 * sessionQuantity is the number of Sessions, which will be generated
+	 *
+	 * @param sessionQuantity The desired sessionQuantity
 	 */
-	public void setEventQuantity(int eventQuantity) {
-		this.eventQuantity = eventQuantity;
+	public void setSessionQuantity(int sessionQuantity) {
+		this.sessionQuantity = sessionQuantity;
+	}
+
+	/**
+	 * workshopQuantity is the number of Workshops, which will be generated
+	 *
+	 * @return The current workshopQuantity
+	 */
+	public int getWorkshopQuantity() {
+		return workshopQuantity;
+	}
+
+	/**
+	 * workshopQuantity is the number of Workshops, which will be generated
+	 *
+	 * @param workshopQuantity The desired workshopQuantity
+	 */
+	public void setWorkshopQuantity(int workshopQuantity) {
+		this.workshopQuantity = workshopQuantity;
 	}
 
 	/**
 	 * Returns the {@link Conference conferences}, which will be added to the conference
-	 * 
+	 *
 	 * @return {@link Conference conferences}, which will be added to the Database
 	 */
 	public Conference[] getConferences() {
@@ -308,7 +358,7 @@ public class JPATestdatabase {
 
 	/**
 	 * Set the {@link Conference conferences}, which will be added to the Database, when {@link #insertData()} is executed}
-	 * 
+	 *
 	 * @param conferences The {@link Conference conferences}, which will be added
 	 */
 	public void setConferences(Conference[] conferences) {
@@ -317,7 +367,7 @@ public class JPATestdatabase {
 
 	/**
 	 * Returns the {@link Institution institutions}, which will be added to the database
-	 * 
+	 *
 	 * @return {@link Institution institutions}, which were originally/will be added to the Database
 	 */
 	public Institution[] getInstitutions() {
@@ -326,7 +376,7 @@ public class JPATestdatabase {
 
 	/**
 	 * Set the {@link Institution institutions}, which will be added to the Database, when {@link #insertData()} is executed}
-	 * 
+	 *
 	 * @param institutions The {@link Institution institutions}, which will be added
 	 */
 	public void setInstitutions(Institution[] institutions) {
@@ -335,7 +385,7 @@ public class JPATestdatabase {
 
 	/**
 	 * Returns the {@link Person authors}, which will be added to the database
-	 * 
+	 *
 	 * @return {@link Person authors}, which were originally/will be added to the database
 	 */
 	public Person[] getAuthors() {
@@ -344,7 +394,7 @@ public class JPATestdatabase {
 
 	/**
 	 * Set the {@link Person authors}, which will be added to the Database, when {@link #insertData()} is executed}
-	 * 
+	 *
 	 * @param authors The {@link Person authors}, which will be added
 	 */
 	public void setAuthors(Person[] authors) {
@@ -353,7 +403,7 @@ public class JPATestdatabase {
 
 	/**
 	 * Returns the {@link Paper papers}, which will be added to the database
-	 * 
+	 *
 	 * @return {@link Paper papers}, which were originally/will be added to the database
 	 */
 	public Paper[] getPapers() {
@@ -362,7 +412,7 @@ public class JPATestdatabase {
 
 	/**
 	 * Set the {@link Paper papers}, which will be added to the Database, when {@link #insertData()} is executed}
-	 * 
+	 *
 	 * @param papers The {@link Paper papers}, which will be added
 	 */
 	public void setPapers(Paper[] papers) {
