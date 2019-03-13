ab3217cefad65093665b935cded672878c0d866d
bl4ckscor3 <donalddani9612@gmx.de>
2019-03-13 16:28:36 +0100

    add comment to explain uncovered branch

--- src/main/java/de/tudarmstadt/informatik/ukp/athena/knowledgebase/database/jpa/WorkshopJPAAccess.java
+++ src/main/java/de/tudarmstadt/informatik/ukp/athena/knowledgebase/database/jpa/WorkshopJPAAccess.java
@@ -22,7 +22,7 @@ public class WorkshopJPAAccess implements CommonAccess<Workshop> {
 		entityManager.getTransaction().begin();
 		try {
 			entityManager.persist(data);
-		}catch(EntityExistsException e) {
+		}catch(EntityExistsException e) {  //branch not tested because exception shouldn't be thrown again just so junit can test for it
 			System.out.println(data.getID()+" already exists in the Database. Maybe try update");
 		}
 		entityManager.getTransaction().commit();
